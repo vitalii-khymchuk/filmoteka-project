fd7f81cdbd71abca5c5f7556dac9166d756c87ab
Vladyslav Hurzhyi <soulline0@gmail.com>
2022-10-27 23:46:45 +0200

    renred pop film

--- src/renderPopularFilm.js
+++ src/renderPopularFilm.js
@@ -11,7 +11,7 @@ export class ThemovieAPI {
   #api = 'c6849c57578619bd16dafe22e211e348';
   #total_pages = '';
   #total_results = '';
-  #page = 4;
+  #page = 6;
 
   async getFilms() {
     const urlAXIOS = `day?api_key=${this.#api}&page=${this.#page}`;
@@ -45,15 +45,65 @@ async function getPopularFilms() {
   try {
     ///spinner
 
-    const { results, page } = await themovieApi.getFilms();
+    const { results } = await themovieApi.getFilms();
     console.log(results);
+    //
+    let imageCard = getAvailabilityImage(results);
+
+    let title = results.map(item => {
+      return item.title;
+    });
+
+    /////////
+    let genresOnCard = checkAndReturnGenres(results);
+    //
+    let releaseDate = sliceDateRelease(results);
+    ///
 
     const parced = JSON.stringify(genres);
     const genresFilm = JSON.parse(parced);
-    console.log(genresFilm);
+    // console.log(genresFilm);
   } catch (error) {
     console.log(error);
   }
 }
 
 getPopularFilms();
+
+//принимает result с бекенда и возвращает ссылки на превью фильмов (если нет то ставит заглушку)
+export function getAvailabilityImage(data) {
+  const image = data.map(item => {
+    if (item.backdrop_path === null) {
+      item.backdrop_path = './images/coverPlaceholder.jpg';
+    } else {
+      item.backdrop_path = `https://image.tmdb.org/t/p/w500/${item.backdrop_path}`;
+    }
+    return item.backdrop_path;
+  });
+  return image;
+}
+
+///проверяет жанры если > 3  обрезает и добавляет Other
+export function checkAndReturnGenres(data) {
+  const genres = data.map(item => {
+    if (item.genre_ids.length > 3) {
+      item.genre_ids = item.genre_ids.slice(0, 4);
+    }
+    return item.genre_ids;
+  });
+
+  let genresOnCardFilter = genres.map(item => {
+    if (item.length > 3) {
+      item[3] = 'Other';
+    }
+    return item;
+  });
+  return genresOnCardFilter;
+}
+
+/////////Обрезает дату релиза
+export function sliceDateRelease(data) {
+  return data.map(item => {
+    return item.release_date.slice(0, 4);
+  });
+}
