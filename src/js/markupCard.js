a70809ec06b269863917d7bf65bd2d268645f9bc
akksinya <akksin.ya999@gmail.com>
2022-10-29 15:23:21 +0300

    add function

--- src/js/markupCard.js
+++ src/js/markupCard.js
@@ -1,6 +1,14 @@
 import cardTpl from '../templates/cardTpl.hbs';
+import pictureExample from '../images/coverPlaceholder.jpg';
 
 export function createMarkupCard (results) {
-    element.innerHtml = cardTpl(results)
+    if(!results[0]) {
+      element.innerHtml = `<img src="${pictureExample}" alt="movie not found"/>`;
+
+    } else {
+        element.innerHtml = cardTpl(results);
+
+    }
+    
     
 }
