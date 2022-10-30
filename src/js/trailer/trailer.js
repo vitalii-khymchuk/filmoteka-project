6193c125a0cbb2d5a7f2e6204e8d5d8512fe7d70
Serhii Malychkovych <masevik@gmail.com>
2022-10-30 18:22:31 +0200

    add fetch trailer

--- src/js/trailer/trailer.js
+++ src/js/trailer/trailer.js
@@ -67,18 +67,6 @@ function onLoadPlayer(data) {
   player.playVideo();
 }
 
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
-}
-
 function stopVideo() {
   player.stopVideo();
 }
