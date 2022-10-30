73b11f2e651de768163bb8df4ca25e06bec81644
Vadym-Khokhlov <vadymkhokhlov1989@gmail.com>
2022-10-30 23:21:00 +0200

    making searchMoviesByName.js

--- src/js/searchMovieByName.js
+++ src/js/searchMovieByName.js
@@ -2,13 +2,8 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
 import { createMarkupCard } from './markupCard';
 import { getActualData } from './markupCard';
+import { refs } from './refs';
 
-export const refs = {
-  searchForm: document.querySelector('.search-form'),
-  cardset: document.querySelector('.card-set'),
-};
-console.dir(refs.searchForm);
-console.log(refs.cardset);
 let page = 1;
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'c6849c57578619bd16dafe22e211e348';
@@ -18,34 +13,39 @@ export async function searchMovieByName(name, page) {
     const response = await axios.get(
       `https://api.themoviedb.org/3/search/movie?api_key=c6849c57578619bd16dafe22e211e348&language=en-US&query=${name}&page=${page}&include_adult=false`
     );
-    console.log(response.data.results);
     return response.data.results;
   } catch (error) {
     console.error(error);
   }
 }
 
-function onSearchSubmit(e) {
+async function onSearchSubmit(e) {
   e.preventDefault();
   page = 1;
-  const movieName = e.currentTarget.searchQuery.value;
-  console.log(movieName);
+  const movieName = e.target.elements.searchQuery.value;
+
+  if (movieName !== '') {
+    try {
+      const result = await searchMovieByName(movieName, page);
+      if (result.length !== 0) {
+        clearMarkup();
+        const results = getActualData(result);
+        createMarkupCard(results);
+      } else {
+        clearMarkup();
+        Notify.failure(
+          'Search result not successful. Enter the correct movie name and try again'
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    Notify.failure('Please, enter your movie name');
+  }
 }
-
-// if (response !== '') {
-//   try {
-//     const result = await searchMovieByName(response, page);
-
-//     if (result.length !== 0) {
-//       console.log(result);
-//     } else {
-//       Notify.failure('error');
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// } else {
-//   Notify.failure('errrrr');
-// }
-
 refs.searchForm.addEventListener('submit', onSearchSubmit);
+
+function clearMarkup() {
+  refs.movieCards.innerHTML = '';
+}
