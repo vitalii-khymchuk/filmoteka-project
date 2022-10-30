269397ce03cf4dfd36cf232fec8903accebaabdd
vitalii.khymchuk <vitalii.workbox001@gmail.com>
2022-10-30 20:23:59 +0200

    set modal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import { initModal } from './js/modal-film/modal-film';
+
 import './js/modal-film/modal-film';
 import './js/pagination';
 import './js/localStorageAPI/renderPopFilm';
 import './js/footer-modal';
 import './js/trailer/trailer';
+
+initModal();
