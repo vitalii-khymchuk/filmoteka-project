3282097e4188bd923c6d08089f9d47d9f0bbb1bc
vitalii.khymchuk <vitalii.workbox001@gmail.com>
2022-10-31 11:16:07 +0200

    start pagination

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { initModal } from './js/modal-film/modal-film';
 
 import './js/modal-film/modal-film';
-import './js/pagination';
 import './js/renderPopularFilm/renderPopFilm';
 import './js/footer-modal';
 import './js/trailer/trailer';
