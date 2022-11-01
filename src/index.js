8bb28304d381e0c0947f0bef6da66e1474e18c5e
vitalii.khymchuk <vitalii.workbox001@gmail.com>
2022-11-01 02:23:49 +0200

    mini refac

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import { initModal } from './js/modal-film/modal-film';
-
-import './js/modal-film/modal-film';
 import './js/renderPopularFilm/renderPopFilm';
 // import './js/footer-modal';
 import './js/trailer/trailer';
 import './js/searchMovieByName';
 import './js/advBlock';
+import './js/buttonUp';
 
 initModal();
